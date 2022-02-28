56e271106e7666d4f07e004f426e3431675ebcb9
cjoshmarshall <cjoshmarshall@gmail.com>
2022-02-28 18:56:11 +0530

    fix 1.3

--- src/actionComment.js
+++ src/actionComment.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 export const comments=(reqObj)=>async dispatch=>{
     console.log('hello')
     try{
-        const response=await axios.post('http://localhost:3006/api/comments', reqObj)
+        const response=await axios.post('http://https://vintagemotorsrentals.herokuapp.com/api/comments', reqObj)
     }
     catch(error){
         console.log(error)        
