e704abf8cf4e26d34750f4e051148d6d6653ce1a
cjoshmarshall <cjoshmarshall@gmail.com>
2022-04-19 12:26:29 +0530

    fix 1.4

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,6 @@ import { Menu, Dropdown } from 'antd';
 function Header() {
      
      const user=JSON.parse(localStorage.getItem("user"))
-     console.log(user)
 
      const logout=()=>{
           localStorage.clear()
