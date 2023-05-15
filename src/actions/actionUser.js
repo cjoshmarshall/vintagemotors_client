1ecde267b91196bda4b20c39d455758ea558873e
Josh Marshall <cjoshmarshall@gmail.com>
2023-05-15 13:30:23 +0530

    Login Token Restriction

--- src/actions/actionUser.js
+++ src/actions/actionUser.js
@@ -1,28 +1,23 @@
-import { publicRequest, userRequest } from '../api/apiHandle';
+import { publicRequest, userRequest } from "../api/apiHandle";
 
+export const userLogin = (reqObj) => async (dispatch) => {
+  try {
+    const res = await publicRequest.post("/auth/login", reqObj);
+    localStorage.setItem("user", JSON.stringify(res.data));
+    window.location.replace("/");
+  } catch (error) {
+    console.log(error);
+    alert("Something went wrong");
+  }
+};
 
-export const userLogin=(reqObj)=>async dispatch=>{
-
-    try{
-        const res=await userRequest.post('/auth/login', reqObj)
-        localStorage.setItem('user', JSON.stringify(res.data))
-        window.location.replace("/")
-    }
-    catch(error){
-        console.log(error)
-        alert('Something went wrong')
-    }
-}
-
-export const userSignup=(reqObj)=>async dispatch=>{
-
-    try{
-        const res=await publicRequest.post('/auth/signup', reqObj)
-        alert('Registration Successful')
-        window.location.replace("/login")
-    }
-    catch(error){
-        console.log(error)        
-        alert('Something went wrong')
-    }
-}
+export const userSignup = (reqObj) => async (dispatch) => {
+  try {
+    const res = await publicRequest.post("/auth/signup", reqObj);
+    alert("Registration Successful");
+    window.location.replace("/login");
+  } catch (error) {
+    console.log(error);
+    alert("Something went wrong");
+  }
+};
